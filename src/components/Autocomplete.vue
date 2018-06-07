797ec1c489cb49e04a89abab0a4541e059aafcd1
Jesray Garciano <jesray.garciano@gmail.com>
2018-06-07 15:56:32 +0800

    Updating Components

---
+++ src/components/Autocomplete.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="autocomplete">
+        <div class="input" @click="toggleVisible"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    date(){
+        return {
+            visible: false
+        };
+    },
+    methods:{
+        toggleVisible(){
+            this.visible = !this.visible;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.autocomplete{
+    width: 100%;
+    position: relative;
+}
+
+</style>
